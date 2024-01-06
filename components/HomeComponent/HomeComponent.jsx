338f55b369ffe7ee8a6a31a9723486ed4bc08ad8
Julian Kisner <julikisner@gmail.com>
2024-01-06 17:25:47 -0300

    production

--- components/HomeComponent/HomeComponent.jsx
+++ components/HomeComponent/HomeComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../Navbar/Navbar'
+import NavBar from '../navbar/Navbar'
 import GifComponent from './GifComponent'
 import Features from './Features'
 import AboutUs from './AboutUs'
@@ -7,7 +7,7 @@ import Footer from '../footer/Footer'
 const HomeComponent = () => {
   return (
     <div className='w-100 h-auto'>
-      <Navbar />
+      <NavBar />
       <GifComponent />
       <Features />
       <AboutUs />
